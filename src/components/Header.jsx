768d5d5373238be3c90f3bb26f00a07a3aa928df
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-28 14:54:55 +0100

    Render Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header>
             <div className="header--container">
-                <i class="fa-solid fa-earth-africa"></i>
+                <i className="fa-solid fa-earth-africa"></i>
                 <h4>my travel journal.</h4>
             </div>
         </header>
