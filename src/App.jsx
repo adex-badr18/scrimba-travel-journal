b5f739a689c672fad71a4bf806739cbd1169e8e3
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-28 16:46:58 +0100

    Render TravelCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Header from './components/Header';
+import TravelCard from './components/TravelCard';
 
 function App() {
   return (
     <div className='container'>
       <Header />
+
+      <section className='travels-list'>
+        <TravelCard />
+      </section>
     </div>
   )
 }
