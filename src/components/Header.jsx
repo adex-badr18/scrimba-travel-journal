62c176ecc8a40e1d502d8980e247a4a3a67481d0
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-28 15:05:52 +0100

    Style Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ export default function Header() {
     return (
         <header>
             <div className="header--container">
-                <i className="fa-solid fa-earth-africa"></i>
-                <h4>my travel journal.</h4>
+                <i className="fa-solid fa-earth-africa header--globe-icon"></i>
+                <h4 className="header--text">my travel journal.</h4>
             </div>
         </header>
     )
