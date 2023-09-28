ba6b76669c41a0d28ecdc4adc700763ca0d4ac62
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-28 18:01:31 +0100

    Render travel data with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
 import Header from './components/Header';
 import TravelCard from './components/TravelCard';
+import travelData from './data';
 
 function App() {
+  const cards = travelData.map(card => {
+    return <TravelCard 
+      key={card.id}
+      {...card}
+    />
+  })
   return (
     <div className='container'>
       <Header />
 
       <section className='travels-list'>
-        <TravelCard />
-        <TravelCard />
+        {cards}
       </section>
     </div>
   )
--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,20 +1,20 @@
-export default function Header() {
+export default function TravelCard(props) {
     return (
         <div className="card">
-            <img src="/images/lake-retba.jpg" alt="Lake Retba" className="card--photo" />
+            <img src={`/images/${props.imageName}`} alt={props.title} className="card--photo" />
 
             <div className="card--info">
                 <div className="card--location">
                     <i className="fa-solid fa-location-dot card--location-icon"></i>
-                    <span className="card--country">Senegal</span>
-                    <a href="#" className="card--map-url">View on Google Maps</a>
+                    <span className="card--country">{props.location}</span>
+                    <a href={props.googleMapsUrl} className="card--map-url">View on Google Maps</a>
                 </div>
 
-                <h1 className="card--name">Lake Retba (Lac Rose)</h1>
+                <h1 className="card--name">{props.title}</h1>
 
-                <h4 className="card--date">12 Jan, 2021 - 24 Jan, 2021</h4>
+                <h4 className="card--date">{props.startDate} - {props.endDate}</h4>
 
-                <p className="card--description">Lake Retba, also called Lac Rose by locals, is a highly saline body of water – one of the highest in the world. The lake gets its color from safe bacteria in the water, which produce a red pigment to absorb sunlight. The best times to see the lake at its pinkest is during Senegal’s dry season, between November and June.</p>
+                <p className="card--description">{props.description}</p>
             </div>
         </div>
     )
