08821947017d51b9298f0936f7ddd959826cd6ae
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-28 14:53:14 +0100

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="header--container">
+                <i class="fa-solid fa-earth-africa"></i>
+                <h4>my travel journal.</h4>
+            </div>
+        </header>
+    )
+}
