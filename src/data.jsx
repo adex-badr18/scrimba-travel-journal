bd5a04ff97104c82b24bb53f530269cbb7adbcd4
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-28 14:17:24 +0100

    Create array of location objects in data.jsx

---
+++ src/data.jsx
@@ -0,0 +1,76 @@
+export default [
+    {
+        title: "Lekki Conservation Centre",
+        location: "Nigeria",
+        googleMapsUrl: "https://maps.app.goo.gl/jGejHSk4iaiBdZFu5",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "A walk far above the ground on Africa’s longest canopy walkway is a great way to explore Nigeria’s natural beauty. The Lekki Conservation Centre offers a serene escape from the hustle and bustle of Lagos, Nigeria’s commercial capital.",
+        imageUrl: "https://unsplash.com/photos/hbNlzEtn-CI"
+    },
+    {
+        title: "Cape Verde Archipelago",
+        location: "Cabo Verde",
+        googleMapsUrl: "https://maps.app.goo.gl/mLrM28PkNWanYWH78",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Located in the North Atlantic and consisting of 10 islands, the Cape Verde archipelago is a cultural blend of African, Iberian and Brazilian influences. From water sports and hikes through volcanic landscapes to leisure times at the scenic beaches, Cape Verde offers it all.",
+        imageUrl: "https://unsplash.com/photos/Tv9zHfesTBQ"
+    },
+    {
+        title: "Lake Retba (Lac Rose)",
+        location: "Senegal",
+        googleMapsUrl: "https://maps.app.goo.gl/K5T2CPcoGr5J1gnn7",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Lake Retba, also called Lac Rose by locals, is a highly saline body of water – one of the highest in the world. The lake gets its color from safe bacteria in the water, which produce a red pigment to absorb sunlight. The best times to see the lake at its pinkest is during Senegal’s dry season, between November and June.",
+        imageUrl: "https://unsplash.com/photos/PnTVEMOJtLo"
+    },
+    {
+        title: "Kilimanjaro",
+        location: "Tanzania",
+        googleMapsUrl: "https://maps.app.goo.gl/NrprHb6qw7htB5Jn9",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Kilimanjaro, at 5,895 meters (19,341 feet), is Africa’s highest peak – and an item on thousands of bucket lists. It is a “Sky island,” creating a varied and dramatic natural habitat. Rising through lush rainforests and alpine meadows, climbers finally cross a barren lunar landscape to reach the twin summit, often above the clouds.",
+        imageUrl: "https://unsplash.com/photos/xev2kb8paN4"
+    },
+    {
+        title: "Fish River Canyon",
+        location: "Namibia",
+        googleMapsUrl: "https://maps.app.goo.gl/v3JfE3xfaD1UxcgN6",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Namibia is the travel gift that keeps on giving. Some 500 meters (1,640 feet) deep and more than 160 kilometers (100 miles) long, this great rift is second only to the United States’ Grand Canyon in size, and during the dry season is characterized by beautiful turquoise pools of seasonally flowing water stretching into the distance.",
+        imageUrl: "https://unsplash.com/photos/hlDQhML3wmI"
+    },
+    {
+        title: "Pyramids of Giza",
+        location: "Egypt",
+        googleMapsUrl: "https://maps.app.goo.gl/Abb5Ex9CoHw5vkEx5",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "The most famous of the structures at Giza, near Cairo, the Pyramid of King Cheops was built around 2650 BC from 2.5 million blocks of limestone. Its sides are oriented exactly to the north, south, east and west.",
+        imageUrl: "https://unsplash.com/photos/00hJ4dTXkw0"
+    },
+    {
+        title: "Table Mountain",
+        location: "South Africa",
+        googleMapsUrl: "https://maps.app.goo.gl/v1wzEAZs88PKwjw6A",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Table Mountain makes Cape Town, one of the world’s best beach cities, also one of the world’s most photogenic. Cable car rides are available to the top of the mesa, giving great views, fantastic sunrises/sunsets and a great photo. Challenge: Try to limit yourself to 50 pics.",
+        imageUrl: "https://unsplash.com/photos/Bmy54VG1j3A"
+    },
+    {
+        title: "Mount Mulanje",
+        location: "Malawi",
+        googleMapsUrl: "https://maps.app.goo.gl/5tYqS7ZZfPQDDSXH6",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "At around 3,000 meters (about 9,850 feet), the Mulanje Massif dwarfs the lush land that surrounds it. It’s split in two by the Fort Lister Gap, a broad pass eroded by the Phalombe and Sombani rivers.",
+        imageUrl: "https://www.istockphoto.com/photo/mountains-in-mulanje-gm1498088899-520239411"
+    },
+]
+
+
