85011182ba97aa2b2eaf5f2a9b035a005f16de26
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-28 20:43:07 +0100

    Add styles for large screen devices

--- src/data.jsx
+++ src/data.jsx
@@ -6,7 +6,7 @@ export default [
         googleMapsUrl: "https://maps.app.goo.gl/jGejHSk4iaiBdZFu5",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "A walk far above the ground on Africa’s longest canopy walkway is a great way to explore Nigeria’s natural beauty. The Lekki Conservation Centre offers a serene escape from the hustle and bustle of Lagos, Nigeria’s commercial capital.",
+        description: "Explore Nigeria's natural beauty on Africa's longest canopy walkway at the Lekki Conservation Centre, providing a serene escape from Lagos' bustling city life.",
         imageUrl: "https://unsplash.com/photos/hbNlzEtn-CI",
         imageName: "lekki-conservation.jpg"
     },
@@ -17,7 +17,7 @@ export default [
         googleMapsUrl: "https://maps.app.goo.gl/mLrM28PkNWanYWH78",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Located in the North Atlantic and consisting of 10 islands, the Cape Verde archipelago is a cultural blend of African, Iberian and Brazilian influences. From water sports and hikes through volcanic landscapes to leisure times at the scenic beaches, Cape Verde offers it all.",
+        description: "Cape Verde, a North Atlantic archipelago with 10 islands. Visitors can enjoy water sports, volcanic hikes, and scenic beaches, making it a versatile destination.",
         imageUrl: "https://unsplash.com/photos/Tv9zHfesTBQ",
         imageName: "cape-verde.jpg"
     },
@@ -28,7 +28,7 @@ export default [
         googleMapsUrl: "https://maps.app.goo.gl/K5T2CPcoGr5J1gnn7",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Lake Retba, also called Lac Rose by locals, is a highly saline body of water – one of the highest in the world. The lake gets its color from safe bacteria in the water, which produce a red pigment to absorb sunlight. The best times to see the lake at its pinkest is during Senegal’s dry season, between November and June.",
+        description: "Lake Retba, or Lac Rose, is a highly saline pink lake in Senegal, thanks to red-pigmented bacteria. It's pinkest between November and June.",
         imageUrl: "https://unsplash.com/photos/PnTVEMOJtLo",
         imageName: "lake-retba.jpg"
     },
@@ -39,7 +39,7 @@ export default [
         googleMapsUrl: "https://maps.app.goo.gl/NrprHb6qw7htB5Jn9",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Mount Kilimanjaro, at 5,895 meters (19,341 feet), is Africa’s highest peak – and an item on thousands of bucket lists. It is a “Sky island,” creating a varied and dramatic natural habitat. Rising through lush rainforests and alpine meadows, climbers finally cross a barren lunar landscape to reach the twin summit, often above the clouds.",
+        description: "Mount Kilimanjaro, Africa's highest peak at 5,895 meters (19,341 feet), offers diverse landscapes, including rainforests and lunar-like terrain, with a twin summit above the clouds.",
         imageUrl: "https://unsplash.com/photos/xev2kb8paN4",
         imageName: "kilimanjaro.jpg"
     },
@@ -50,7 +50,7 @@ export default [
         googleMapsUrl: "https://maps.app.goo.gl/v3JfE3xfaD1UxcgN6",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Namibia is the travel gift that keeps on giving. Some 500 meters (1,640 feet) deep and more than 160 kilometers (100 miles) long, this great rift is second only to the United States’ Grand Canyon in size, and during the dry season is characterized by beautiful turquoise pools of seasonally flowing water stretching into the distance.",
+        description: "Namibia's Fish River Canyon rivals the Grand Canyon in size, with a 500-meter depth and 160-kilometer length. In the dry season, it showcases beautiful turquoise pools.",
         imageUrl: "https://unsplash.com/photos/hlDQhML3wmI",
         imageName: "fish-river-canyon.jpg"
     },
@@ -61,7 +61,7 @@ export default [
         googleMapsUrl: "https://maps.app.goo.gl/Abb5Ex9CoHw5vkEx5",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "The most famous of the structures at Giza, near Cairo, the Pyramid of King Cheops was built around 2650 BC from 2.5 million blocks of limestone. Its sides are oriented exactly to the north, south, east and west.",
+        description: "The Pyramid of King Cheops near Cairo, built around 2650 BC with 2.5 million limestone blocks, has sides aligned to the cardinal directions.",
         imageUrl: "https://unsplash.com/photos/00hJ4dTXkw0",
         imageName: "pyramids-of-giza.jpg"
     },
@@ -72,7 +72,7 @@ export default [
         googleMapsUrl: "https://maps.app.goo.gl/v1wzEAZs88PKwjw6A",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Table Mountain makes Cape Town, one of the world’s best beach cities, also one of the world’s most photogenic. Cable car rides are available to the top of the mesa, giving great views, fantastic sunrises/sunsets and a great photo. Challenge: Try to limit yourself to 50 pics.",
+        description: "Table Mountain in Cape Town is incredibly photogenic, with breathtaking views and sunsets. Take the cable car to the top, but try to limit yourself to 50 photos.",
         imageUrl: "https://unsplash.com/photos/Bmy54VG1j3A",
         imageName: "table-mountain.jpg"
     },
@@ -83,7 +83,7 @@ export default [
         googleMapsUrl: "https://maps.app.goo.gl/5tYqS7ZZfPQDDSXH6",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "At around 3,000 meters (about 9,850 feet), the Mulanje Massif dwarfs the lush land that surrounds it. It’s split in two by the Fort Lister Gap, a broad pass eroded by the Phalombe and Sombani rivers.",
+        description: "At 3,000 meters (9,850 feet), the Mulanje Massif dominates the surrounding landscape, split in two by the Fort Lister Gap.",
         imageUrl: "https://www.istockphoto.com/photo/mountains-in-mulanje-gm1498088899-520239411",
         imageName: "mount-mulanje.jpg"
     },
